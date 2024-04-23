09a99828fa9ee3fc9a0fcd5b05e2e92d1d8d58c7
JuaneDev <121892094+juaneprograming@users.noreply.github.com>
2024-04-23 22:02:52 +0000

    carpeta img creada

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,12 +1,12 @@
-import Link from "next/link"
-import Image from "next/image"
+import Link from "next/link";
+import Image from "next/image";
 
 
 
-const logo = () => {
+const Logo = () => {
   return  <Link href='/'>
     <Image src='/' width={54} height={54} priority alt="" />
   </Link>
 }
 
-export default logo
+export default Logo

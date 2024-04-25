a9c38ea4db840b5e8e7ddd5175573881f25acfa7
JuaneDev <121892094+juaneprograming@users.noreply.github.com>
2024-04-25 21:50:59 +0000

    botones de redes sociales hechos y color del proyecto cambiado

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -35,6 +35,8 @@ const Hero = () => {
                         <Button variant='secondary' className="gap-x-2">Descargar CV <Download size={18}/>
                         </Button>
                     </div>
+                    {/*socials*/}
+                    <Socials containerStyles='flex gap-x-6 mx-auto xl:mx-0' iconsStyles="text-foreground text-[22px] hover:text-primary transition-all"/>
                 </div>
                 {/*imagen*/}
                 <div className="hidden xl:flex relative">image</div>
--- src/components/Socials.tsx
+++ src/components/Socials.tsx
@@ -1,8 +1,34 @@
-import React from 'react'
+'use client'
+import { RiLinkedinFill, RiGithubFill, RiInstagramFill, RiFacebookFill } from 'react-icons/ri'
+import Link from 'next/link'
 
-const Socials = () => {
-  return <div>Socials</div>
-  
+interface IconProps {
+  path: string
+  icon: React.ReactNode
+}
+
+const icons: IconProps[] = [
+  { path: '/', icon: <RiLinkedinFill /> },
+  { path: '/', icon: <RiGithubFill /> },
+  { path: '/', icon: <RiInstagramFill /> },
+  { path: '/', icon: <RiFacebookFill /> },
+]
+
+interface SocialsProps {
+  containerStyles?: string
+  iconsStyles?: string
+}
+
+const Socials: React.FC<SocialsProps> = ({ containerStyles, iconsStyles }) => {
+  return (
+    <div className={`${containerStyles}`}>
+      {icons.map((icon, index) => (
+        <Link href={icon.path} key={index}>
+          <div className={`${iconsStyles}`}>{icon.icon}</div>
+        </Link>
+      ))}
+    </div>
+  )
 }
 
 export default Socials

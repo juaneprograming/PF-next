4efd0548fa70fadd423604fffe75288595869cb5
ojuan2594 <ojuan2594@gmail.com>
2024-05-15 15:46:04 +0000

    services terminado y logo editado

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 
 const Logo = () => {
   return  <Link href='/'>
-    <Image src='/image/logo.png' width={54} height={54} priority alt="" />
+    <Image src='/image/logo_white.png' width={54} height={54} priority alt="" />
   </Link>
 }
 
--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -1,9 +1,9 @@
-import {GanttChartSquare, Blocks, Gem} from 'lucide-react'
+import { GanttChartSquare, Blocks, Gem } from 'lucide-react'
 import {
-    Card, 
-    CardContent, 
-    CardDescription, 
-    CardHeader, 
+    Card,
+    CardContent,
+    CardDescription,
+    CardHeader,
     CardTitle
 } from '@/components/ui/card'
 
@@ -26,22 +26,27 @@ const servicesData = [
 ]
 
 const Services = () => {
-  return <section className='mb-12 xl:mb-36'>
-    <div className='container mx-auto'>
-        <h2 className='section-title mb-12 xl:mb-24 mx-auto'>My services</h2>
-        {/* grid items */}
-        <div className='grid xl:grid-cols-3 justify-center gap-y-12 xl:gap-y-24 xl:gap-x-8'>
-            {servicesData.map((item, index)=> {
-                return <Card className='w-full max-w-[424px] h-[300px] flex flex-col pt-16 pb-10 justify-center items-center relative' key={index}>
-                    <CardHeader className='text-primary absolute -top-[60px]'>
-                        <div className='w-[148px] h-[88px] bg-pink-100 dark:bg-background flex justify-center items-center'>{item.icon}</div>
-                    </CardHeader>
-                </Card>
-            })}
+    return <section className='mb-12 xl:mb-36'>
+        <div className='container mx-auto'>
+        <h2 className='section-title mb-12 xl:mb-24 mx-auto text-center sm:text-left'>My services</h2>
+            {/* grid items */}
+            <div className='grid xl:grid-cols-3 justify-center gap-y-12 xl:gap-y-24 xl:gap-x-8'>
+                {servicesData.map((item, index) => {
+                    return <Card className='w-full max-w-[424px] h-[300px] flex flex-col pt-16 pb-10 justify-center items-center relative'
+                        key={index}>
+                        <CardHeader className='text-primary absolute -top-[60px]'>
+                            <div className='w-[148px] h-[88px] bg-white dark:bg-background flex justify-center items-center'>{item.icon}</div>
+                        </CardHeader>
+                        <CardContent className='text-center'>
+                            <CardTitle className='mb-4'>{item.title}</CardTitle>
+                            <CardDescription className='text-lg'>{item.description}</CardDescription>
+                        </CardContent>
+                    </Card>
+                })}
+            </div>
         </div>
-    </div>
-  </section>
-  
+    </section>
+
 }
 
 export default Services

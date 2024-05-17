96c7458591ae3ef8c0be9960b4a235299a2e7373
ojuan2594 <ojuan2594@gmail.com>
2024-05-17 19:50:43 +0000

    cards hechas por fin

--- src/components/ProjetctCard.tsx
+++ src/components/ProjetctCard.tsx
@@ -3,20 +3,25 @@ import Link from 'next/link'
 import { Card, CardHeader } from './ui/card'
 import { Github, Link2Icon} from 'lucide-react'
 import { Badge } from './ui/badge'
-
-
-
-const ProjetctCard = ({ project }) => {
-  return (
-  <Card>
-    <CardHeader>card header</CardHeader>
-    <div className='h-full px-8 py-6'>
-      <Badge>{project.category}</Badge>
-      <h4 className='h4 mb-1'>{project.name}</h4>
-      <p className='text-muted-foreground text-lg'>{project.description}</p>
-    </div>
-  </Card>
-  )
+interface Project {
+  category
+: string;
+  name: string;
+  description: string;
 }
 
-export default ProjetctCard
+
+const ProjectCard = ({ project }: { project: Project }) => {
+  return (
+    <Card>
+      <CardHeader>card header</CardHeader>
+      <div className='h-full px-8 py-6'>
+        <Badge>{project.category}</Badge>
+        <h4 className='h4 mb-1'>{project.name}</h4>
+        <p className='text-muted-foreground text-lg'>{project.description}</p>
+      </div>
+    </Card>
+  );
+};
+
+export default ProjectCard

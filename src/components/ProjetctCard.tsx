1b829d97285e9b3572584297873f6cac69f7486c
ojuan2594 <ojuan2594@gmail.com>
2024-05-17 00:37:38 +0000

    cards hechas

--- src/components/ProjetctCard.tsx
+++ src/components/ProjetctCard.tsx
@@ -1,8 +1,22 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import { Card, CardHeader } from './ui/card'
+import { Github, Link2Icon} from 'lucide-react'
+import { Badge } from './ui/badge'
 
 
-const ProjetctCard = ({project}) => {
-  return <div>ProjetctCard</div>
-  
+
+const ProjetctCard = ({ project }) => {
+  return (
+  <Card>
+    <CardHeader>card header</CardHeader>
+    <div className='h-full px-8 py-6'>
+      <Badge>{project.category}</Badge>
+      <h4 className='h4 mb-1'>{project.name}</h4>
+      <p className='text-muted-foreground text-lg'>{project.description}</p>
+    </div>
+  </Card>
+  )
 }
 
 export default ProjetctCard
--- src/components/Work.tsx
+++ src/components/Work.tsx
@@ -34,7 +34,7 @@ const projectData = [
 
 
 const Work = () => {
-  return <section className='realtive mb-12 xl:mb-48'>
+  return <section className='relative mb-12 xl:mb-48'>
     <div className="container mx-auto">
         {/* texto */}
         <div className='max-w-[400px] mx-auto xl:mx-0 text-center xl:text-left mb-12 xl:h-[400px] flex flex-col justify-center items-center xl:items-start'>
@@ -45,8 +45,8 @@ const Work = () => {
                 </Link>
         </div>
         {/* sliders */}
-        <div className='xl:max-w-[1000px]'>
-            <Swiper>
+        <div className='xl:max-w-[1000px] xl:absolute right-0 top-0'>
+            <Swiper className='h-[480px]' slidesPerView={1} spaceBetween={20} pagination={{clickable: true}} modules={[Pagination]}>
                 {/* muestra los dos primeros proyectos para el slide */}
                 {projectData.slice(0.4).map((project, index)=>{
                     return <SwiperSlide key={index}>

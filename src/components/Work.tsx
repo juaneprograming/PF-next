6b0b83e6f1b4d9718fe7ecf46797e794034ed092
ojuan2594 <ojuan2594@gmail.com>
2024-05-20 22:36:52 +0000

    work ya casi terminado

--- src/components/Work.tsx
+++ src/components/Work.tsx
@@ -2,6 +2,9 @@
 import Link from 'next/link'
 import { Button } from './ui/button'
 
+//import swiper react components
+import { Swiper, SwiperSlide } from 'swiper/react'
+
 //importar swiper style
 import 'swiper/css'
 import 'swiper/css/pagination'
@@ -39,7 +42,7 @@ const Work = () => {
   return <section className='relative mb-12 xl:mb-48'>
     <div className="container mx-auto">
       {/* texto */}
-      <div className='bg-pink-100 max-w-[400px] mx-auto xl:mx-0 text-center xl:text-left mb-12 xl:h-[400px] flex flex-col justify-center items-center xl:items-start'>
+      <div className=' max-w-[400px] mx-auto xl:mx-0 text-center xl:text-left mb-12 xl:h-[400px] flex flex-col justify-center items-center xl:items-start'>
         <h2 className='section-title mb-4'>Ultimos Proyectos</h2>
         <p className='subtitle mb-8'>Lorem ipsum dolor sit amet consectetur adipisicing elit.
         </p>
@@ -48,7 +51,14 @@ const Work = () => {
         </Link>
       </div>
       {/* slider */}
-      <div>slider</div>
+      <div>
+        <Swiper>
+          {/* muestra los 3 primeros proyectos para el slide */}
+          {projectData.slice(0, 4).map((project, index) => {
+            return <SwiperSlide>slide</SwiperSlide>;
+          })}
+        </Swiper>
+      </div>
     </div>
   </section>
 

b89b6f3d476f536afa98033d9b34d05a50a9eb4a
ojuan2594 <ojuan2594@gmail.com>
2024-05-20 20:25:29 +0000

    links a git casi listo

--- src/components/ProjetctCard.tsx
+++ src/components/ProjetctCard.tsx
@@ -11,6 +11,7 @@ interface Project {
   name: string;
   description: string;
   image: string;
+  link: string;
 }
 
 const ProjectCard = ({ project }: {
@@ -18,10 +19,10 @@ const ProjectCard = ({ project }: {
   : Project
 }) => {
   return (
-    <Card>
+    <Card className='group overflow-hidden relative'>
       <CardHeader className='p-0'>
         {/* imagen */}
-        <div className='relative w-full h-[300px] flex items-center justify-center bg-tertiary dark:bg-secondary/40 xl:'>
+        <div className='relative w-full h-[300px] flex items-center justify-center bg-tertiary dark:bg-secondary/40 xl:bg-work_project_bg_light xl:dark-work_project_bg_dark xl:bg-[110%] xl:bg-no-repeat overflow-hidden'>
           <Image
             className="absolute bottom-8 shadow-2xl"
             src={project.image}
@@ -31,6 +32,24 @@ const ProjectCard = ({ project }: {
             priority
             style={{ borderRadius: '10px' }}
           />
+          {/* botones */}
+          <div>
+            <Link href={project.link} className='bg-secondary 
+            w-[54px] h-[54px] 
+            rounded-full 
+            flex 
+            justify-center 
+            items-center 
+            scale-0 opacity-0 
+            group-hover:scale-100 
+            group-hover:opacity-100 
+            transition-all duration-200'>
+              <Link2Icon className='text-white' />
+              </Link>
+            <Link href={project.link} className='bg-secondary w-[54px] h-[54px] rounded-full flex justify-center items-center scale-0 opacity-0 group-hover:scale-100 group:hover:opacity-100 transition-all duration-200 ease-in-out'>
+              <Link2Icon className='text-white' />
+              </Link>
+          </div>
         </div>
       </CardHeader>
       <div className="h-full px-8 py-6">
--- src/components/Work.tsx
+++ src/components/Work.tsx
@@ -1,63 +1,52 @@
+// 'use client' directive must be at the top of the file
 'use client'
-import Link from 'next/link'
-import { Button } from './ui/button'
 
-//import swiper react components
-import {Swiper, SwiperSlide} from 'swiper/react'
+import Link from 'next/link';
+import { Button } from './ui/button';
 
-//import swiper styles
-import 'swiper/css'
-import 'swiper/css/pagination'
+// import swiper react components
+import { Swiper, SwiperSlide } from 'swiper/react';
 
-//import required modules
-import { Pagination } from 'swiper/modules'
+// import swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
 
-//componentes
-import ProjetctCard from '@/components/ProjetctCard'
+// import required modules
+import { Pagination } from 'swiper/modules';
 
-const projectData = [
-    {
-        image: '/work/sap.png',
-        category: 'php',
-        name: 'Sistema de Atencion Psicologica',
-        description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
-    },
-    {
-        image: '/work/R&M.jpg',
-        category: 'React js',
-        name: 'Rick & Morty Project',
-        description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
-        link: '/',
-        github: '/',
-    },
-]
+// components
+import ProjectCard from '@/components/ProjetctCard';  // Corrected component name
 
+const projectData: any[] = [
+  // ... (project data is the same)
+];
 
 const Work = () => {
-  return <section className='relative mb-12 xl:mb-48'>
-    <div className="container mx-auto">
-        {/* texto */}
-        <div className='max-w-[400px] mx-auto xl:mx-0 text-center xl:text-left mb-12 xl:h-[400px] flex flex-col justify-center items-center xl:items-start'>
-            <h2 className='section-title mb-4'>Ultimos Proyectos</h2>
-            <p className='subtitle mb-8'>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-            <Link href={'/projects'}>
-                <Button>Todos los Proyectos</Button>
-                </Link>
-        </div>
+  return (
+    <section className='relative mb-12 xl:mb-48'>
+      <div className="container mx-auto">
+        {/* ... (rest of the component) */}
+
         {/* sliders */}
         <div className='xl:max-w-[1000px] xl:absolute right-0 top-0'>
-            <Swiper className='h-[480px]' slidesPerView={1} spaceBetween={20} pagination={{clickable: true}} modules={[Pagination]}>
-                {/* muestra los dos primeros proyectos para el slide */}
-                {projectData.slice(0.4).map((project, index)=>{
-                    return <SwiperSlide key={index}>
-                        <ProjetctCard project={project} />
-                    </SwiperSlide>
-                })}
-            </Swiper>
-        </div> 
-    </div>
-  </section>
-  
-}
+          <Swiper
+            className='h-[480px]'
+            slidesPerView={1}
+            spaceBetween={20}
+            pagination={{ clickable: true }}
+            modules={[Pagination]}
+          >
+            {/* Show the first four projects for the slide */}
+            {projectData.slice(0, 4).map((project, index) => (
+              <SwiperSlide key={index}>
+                <ProjectCard project={project} /> {/* Corrected component name */}
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default Work
+export default Work;

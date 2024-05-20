2eb2593dcab13725fcfde1b5539e2c99bcd01939
ojuan2594 <ojuan2594@gmail.com>
2024-05-20 21:15:19 +0000

    correciones de la vista work y cartas

--- src/components/Work.tsx
+++ src/components/Work.tsx
@@ -1,52 +1,57 @@
-// 'use client' directive must be at the top of the file
 'use client'
+import Link from 'next/link'
+import { Button } from './ui/button'
 
-import Link from 'next/link';
-import { Button } from './ui/button';
+//importar swiper style
+import 'swiper/css'
+import 'swiper/css/pagination'
 
-// import swiper react components
-import { Swiper, SwiperSlide } from 'swiper/react';
+//import required modules
+import { Pagination } from 'swiper/modules'
 
-// import swiper styles
-import 'swiper/css';
-import 'swiper/css/pagination';
+//componentes
+import ProjectCard from './ProjectCard'
 
-// import required modules
-import { Pagination } from 'swiper/modules';
-
-// components
-import ProjectCard from '@/components/ProjetctCard';  // Corrected component name
-
-const projectData: any[] = [
-  // ... (project data is the same)
+const projectData = [
+  {
+    image: '/work/sap.png',
+    category: 'PHP',
+    name: 'SAP',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+  },
+  {
+    image: '/work/R&M.png',
+    category: 'React Js',
+    name: 'Rick & Morty App',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+    link: '/',
+    github: '',
+  },
+  {
+    image: '/work/sap.png',
+    category: 'Next Js',
+    name: 'Dashboard',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+  },
 ];
 
 const Work = () => {
-  return (
-    <section className='relative mb-12 xl:mb-48'>
-      <div className="container mx-auto">
-        {/* ... (rest of the component) */}
-
-        {/* sliders */}
-        <div className='xl:max-w-[1000px] xl:absolute right-0 top-0'>
-          <Swiper
-            className='h-[480px]'
-            slidesPerView={1}
-            spaceBetween={20}
-            pagination={{ clickable: true }}
-            modules={[Pagination]}
-          >
-            {/* Show the first four projects for the slide */}
-            {projectData.slice(0, 4).map((project, index) => (
-              <SwiperSlide key={index}>
-                <ProjectCard project={project} /> {/* Corrected component name */}
-              </SwiperSlide>
-            ))}
-          </Swiper>
-        </div>
+  return <section className='relative mb-12 xl:mb-48'>
+    <div className="container mx-auto">
+      {/* texto */}
+      <div className='bg-pink-100 max-w-[400px] mx-auto xl:mx-0 text-center xl:text-left mb-12 xl:h-[400px] flex flex-col justify-center items-center xl:items-start'>
+        <h2 className='section-title mb-4'>Ultimos Proyectos</h2>
+        <p className='subtitle mb-8'>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        </p>
+        <Link href='/projects'>
+          <Button>Todos los Proyectos</Button>
+        </Link>
       </div>
-    </section>
-  );
-};
+      {/* slider */}
+      <div>slider</div>
+    </div>
+  </section>
 
-export default Work;
+}
+
+export default Work

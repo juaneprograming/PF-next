62db20cbe503dfd27ec15231aed480e5162b7b5b
ojuan2594 <ojuan2594@gmail.com>
2024-05-28 19:35:39 +0000

    creando el contact me

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import { MailIcon, HomeIcon, PhoneIcon } from "lucide-react";
 
 const Contact = () => {
-  return <div>Contact page</div>
-  
-}
+  return (
+    <section>
+      <div className="container mx-auto">
+        {/* texto y ilustracion */}
+        <div>
+          {/* texto */}
+          <div className="flex flex-col justify-center">
+            <div className="flex items-center gap-x-4 text-primary text-lg mb-4">
+              <span className="w-[30px] h-[2px] bg-primary"></span>
+              Holaaaaaaaaaaaaaaa,👋🏽
+            </div>
+            <h1 className="h1">Vamos a Trabajar Juntos</h1>
+          </div>
+        </div>
+        {/*  */}
+        <div>Texto y formulario</div>
+      </div>
+    </section>
+  );
+};
 
-export default Contact
+export default Contact;

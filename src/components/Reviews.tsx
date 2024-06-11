ff7c9a87eac2ace592f147fea33ff4e805e69a94
JuaneDev <121892094+juaneprograming@users.noreply.github.com>
2024-06-11 23:36:18 +0000

    cambiando textos a español

--- src/components/Reviews.tsx
+++ src/components/Reviews.tsx
@@ -37,7 +37,7 @@ const reviewData = [
 const Reviews = () => {
   return <section className='mb-12 xl:mb-32'>
     <div className="container mx-auto">
-        <h2 className='section-title mb-12 text-center mx-auto'>Reviews</h2>
+        <h2 className='section-title mb-12 text-center mx-auto'>Reseñas</h2>
         {/* slider */}
         <Swiper slidesPerView={1} breakpoints={{
             640: { slidesPerView: 2},
